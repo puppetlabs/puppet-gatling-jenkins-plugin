64259dbfc20a8e0efc06a28102a70684a4171b3a
Chris Price <chris@puppetlabs.com>
2016-08-13 22:09:10 -0700

    (SERVER-1468) Skeleton test for pipeline step

---
+++ src/test/java/com/puppetlabs/jenkins/plugins/puppetgatling/steps/PuppetGatlingArchiverStepTest.java
@@ -0,0 +1,35 @@
+package com.puppetlabs.jenkins.plugins.puppetgatling.steps;
+
+import org.apache.commons.lang.StringUtils;
+import org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition;
+import org.jenkinsci.plugins.workflow.job.WorkflowJob;
+import org.jenkinsci.plugins.workflow.job.WorkflowRun;
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.jvnet.hudson.test.JenkinsRule;
+
+import java.util.Arrays;
+
+public class PuppetGatlingArchiverStepTest extends Assert {
+    @Rule
+    public JenkinsRule j = new JenkinsRule();
+
+    /**
+     * Test archiving of gatling reports
+     */
+    @Test
+    public void archive() throws Exception {
+        // job setup
+        WorkflowJob foo = j.jenkins.createProject(WorkflowJob.class, "foo");
+        foo.setDefinition(new CpsFlowDefinition(StringUtils.join(Arrays.asList(
+                "node {",
+                "  echo 'hi'",
+                "}"), "\n")));
+
+        // get the build going, and wait until workflow pauses
+        WorkflowRun b = j.assertBuildStatusSuccess(foo.scheduleBuild2(0).get());
+
+        assertEquals(true, true);
+    }
+}
