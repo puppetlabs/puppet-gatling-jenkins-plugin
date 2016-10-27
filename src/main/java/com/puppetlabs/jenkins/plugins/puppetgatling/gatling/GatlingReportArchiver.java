149d4560690e91715e8ad240c0ee19c2c12db23d
Chris Price <chris@puppetlabs.com>
2016-10-27 15:57:53 -0700

    (SERVER-1604) Compress gatling simulation.log files
    This commit adds some logic to the step where we are archiving the
    gatling report directory, to gzip the `simulation.log` file before
    archiving it.  This will allow us to retain more build history
    on the jenkins server, since these files can be very large.

--- src/main/java/com/puppetlabs/jenkins/plugins/puppetgatling/gatling/GatlingReportArchiver.java
+++ src/main/java/com/puppetlabs/jenkins/plugins/puppetgatling/gatling/GatlingReportArchiver.java
@@ -6,10 +6,12 @@ import io.gatling.jenkins.*;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.zip.GZIPOutputStream;
 
 /**
  * NOTE: this class consists almost entirely of code copy/pasted out of GatlingPublisher,
@@ -69,7 +71,12 @@ public class GatlingReportArchiver {
 
             FilePath reportDirectory = new FilePath(simulationDirectory);
 
+            FilePath simulationLogFile = reportToArchive.child("simulation.log");
+            gzipFilePath(logger, simulationLogFile);
+
             reportToArchive.copyRecursiveTo(reportDirectory);
+
+            logger.println("Copying report from '" + reportToArchive + "' to '" + reportDirectory + "'");
 
             io.gatling.jenkins.SimulationReport report = new io.gatling.jenkins.SimulationReport(reportDirectory, simulation);
             report.readStatsFile();
@@ -100,4 +107,25 @@ public class GatlingReportArchiver {
         }
         return reportsFromThisBuild;
     }
+
+    private void gzipFilePath(PrintStream logger, FilePath filePath) throws IOException, InterruptedException {
+        byte[] buffer = new byte[1024 * 1024];
+
+        FilePath outFilePath = filePath.getParent().child(filePath.getName() + ".gz");
+        logger.println("Compressing '" + filePath + "' to '" + outFilePath + "'");
+
+        GZIPOutputStream outStream = new GZIPOutputStream(outFilePath.write());
+        InputStream inStream = filePath.read();
+
+        int len;
+        while ((len = inStream.read(buffer)) > 0) {
+            outStream.write(buffer, 0, len);
+        }
+        inStream.close();
+        outStream.finish();
+        outStream.close();
+        logger.println("Compression successful, deleting original file.");
+        filePath.delete();
+    }
+
 }
